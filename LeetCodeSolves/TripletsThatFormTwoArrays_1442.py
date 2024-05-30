6629fa34987fda2158f9b3b62e422bdb4d3b9a47
Sushanth C <sushanth15900@gmail.com>
2024-05-30 11:24:56 +0530

    [SOLVED] 1442. Count Triplets That Can Form Two Arrays of Equal XOR (30/05/2024)

---
+++ LeetCodeSolves/TripletsThatFormTwoArrays_1442.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/
+
+def countTriplets(arr):
+    N = len(arr)
+    res = 0
+    for i in range(N - 1):
+        curr_xor = arr[i]
+        for k in range(i + 1, N):
+            curr_xor ^= arr[k]
+            if curr_xor == 0:
+                res += (k - i)
+    return res
+
+
+if __name__ == '__main__':
+    arr = [1,1,1,1,1]
+    print(countTriplets(arr))
